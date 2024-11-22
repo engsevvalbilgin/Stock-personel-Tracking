b0acbe24d95e9405a462a6e564558adb95018ce8
Şevval Bilğin <eng.sevval.bilgin@gmail.com>
2024-11-22 19:01:12 +0300

    update1

---
+++ src/main/java/com/mycompany/sevval_bilgin_stock_personel_tracking/Stock_personel_tracking.java
@@ -0,0 +1,13 @@
+//Şevval Bilğin 2221221013
+
+package com.mycompany.sevval_bilgin_stock_personel_tracking;
+
+/**
+ *
+ * @author hp
+ */
+public class Stock_personel_tracking {
+
+    public static void main(String args[]) {
+       new LogInFrame().setVisible(true);
+}}
